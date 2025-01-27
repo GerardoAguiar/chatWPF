6d2beb190963378290cbb1a03f2346012d9c71b1
Gerardo Aguiar <79877702+GerardoAguiar@users.noreply.github.com>
2025-01-27 11:16:03 -0600

    ChatServer
    -Create API proyect
    -Create HUB
    -Crete ChatHub class
    -Configure SignalR

---
+++ API/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // Método para enviar mensajes a todos los clientes conectados
+        public async Task SendMessage(string user, string message)
+        {
+            // Envía el mensaje a todos los clientes
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
