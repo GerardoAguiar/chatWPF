001980fdb6e7f73e6c3a549abc8bd24e9f5b7497
Gerardo Aguiar <79877702+GerardoAguiar@users.noreply.github.com>
2025-01-27 10:53:36 -0600

    First pull

---
+++ ChatTest/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string message = MessageTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                MessageListBox.Items.Add($"Tú: {message}");
+                MessageTextBox.Clear();
+            }
+        }
+    }
+}
